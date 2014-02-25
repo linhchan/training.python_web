f523b1a3b32260fdba0669d4a812458d726a70eb
linhchan <linhchan1205@gmail.com>
2014-02-25 12:24:04 -0800

    Linh Tran HW7 submission

--- assignments/session07/mysite/myblog/admin.py
+++ assignments/session07/mysite/myblog/admin.py
@@ -1,6 +1,31 @@
 from django.contrib import admin
-from myblog.models import Post
-from myblog.models import Category
+from myblog.models import Post, Category
 
-admin.site.register(Post)
-admin.site.register(Category)
+# class AuthorAdmin(admin.ModelAdmin):
+#     fields = ('name', 'title', 'url')
+
+class CatergoryInline(admin.TabularInline):
+    model = Category.posts.through
+
+
+class CategoryAdmin(admin.ModelAdmin):
+	fields = ('name','description')
+	exclude = ('posts',)
+
+class PostAdmin(admin.ModelAdmin):
+	# def make_published(self, request, queryset):
+	# 	queryset.update(status='p')
+
+	# def make_unpublished(self, request, queryset):
+	# 	queryset.update(status='d')
+
+	list_display = ['text', 'author', 'created_date', 'modified_date', 'published_date']
+	readonly_fields = ('created_date', 'modified_date')
+	fields = ['text', 'author','created_date', 'modified_date', 'published_date' ]
+	inlines = [CatergoryInline, ]
+	#actions = ['make_published','make_unpublished']
+	#make_published.short_description = "Publish selected posts"
+
+#admin.site.register(Author, AuthorAdmin)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
--- assignments/session07/mysite/myblog/models.py
+++ assignments/session07/mysite/myblog/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+# STATUS_CHOICES = (
+#     ('d', 'Draft'),
+#     ('p', 'Published'),
+# )
+
 class Post(models.Model):
 	title = models.CharField(max_length=128)
 	text = models.TextField(blank=True)
@@ -8,6 +13,8 @@ class Post(models.Model):
 	created_date = models.DateTimeField(auto_now_add=True)
 	modified_date = models.DateTimeField(auto_now=True)
 	published_date = models.DateTimeField(blank=True, null=True)
+	# I'm sorting out some errors before doing second submission.
+	#status = models.CharField(max_length=1, choices=STATUS_CHOICES)
 
 	def __unicode__(self):
 		return self.title
@@ -20,3 +27,13 @@ class Category(models.Model):
 
     def __unicode__(self):
         return self.name
+
+    class Meta():
+    	verbose_name_plural = 'Categories'
+
+# class Author(models.Model):
+#     name = models.CharField(max_length=100)
+#     title = models.CharField(max_length=3)
+#     created_date = models.DateTimeField(auto_now_add=True)
+#     def __unicode__(self):
+#     	return self.name
